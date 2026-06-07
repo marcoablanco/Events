1c7f6b4def97412595fb1989e0baf0c4a716b8ff
Marcos Antonio Blanco <marcoablanco@outlook.com>
2026-06-07 19:45:31 +0200

    WIP

--- 2026-04 GDG Jerez/SampleMaui/SampleMaui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SampleMaui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
